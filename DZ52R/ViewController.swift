d1004b8fd6e804202be603f9539aff6a3a04a9f4
Memka <mkasenova87@gmail.com>
2023-05-23 23:11:39 +0600

    Initial Commit

---
+++ DZ52R/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DZ52R
+//
+//  Created by merim kasenova on 23/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
